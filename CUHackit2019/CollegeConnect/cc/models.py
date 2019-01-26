e1863b60fd00b4570478400a9c435f2f7208e012
Qwelian D. Tanner <dmtorcode@gmail.com>
2019-01-26 13:53:42 -0500

    models and stuff

---
+++ CUHackit2019/CollegeConnect/cc/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class CollegeConnect(models.Model):
+
+class Users(models.Model):
+
+class Skill(models.Model):
+
+class Groups(models.Model):
+
+class Classes(models.Model):
+
+class Questions(models.Model):
+
+class Tutors(models.Model):
