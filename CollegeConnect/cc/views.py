5f612d1ba2279bf2fd2d969df635c72f78af5c69
Damali Boyd <damaliboyd@ymail.com>
2019-01-26 16:12:11 -0500

    Update views.py
    views

--- CollegeConnect/cc/views.py
+++ CollegeConnect/cc/views.py
@@ -14,3 +14,19 @@ class PasswordResetView(auth_views.PasswordResetView):
 class LogoutView(auth_views.LogoutView):
 	template_name = 'logout.html'
 	next_page = reverse_lazy('/login')
+
+class CCHomeView(TemplateView):
+    template_name = "index.html"
+
+class CCDetailView(DetailView):
+    model = User
+	class CCCreateView(UpdateView):
+	model = User
+
+class CCListView(ListView):
+    model = User
+
+class CCDeleteView(DeleteView):
+    model = User
+    success_url = reverse_lazy('animal_list')
+
