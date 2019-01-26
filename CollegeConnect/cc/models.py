af09c111b2cb3a8246c2d891ec935bf553d02529
Teresa <teresachenec@gmail.com>
2019-01-26 16:07:37 -0500

    AutoOneToOneField added

--- CollegeConnect/cc/models.py
+++ CollegeConnect/cc/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
+from annoying.fields import AutoOneToOneField
 from django.urls import reverse_lazy
 
 # Create your models here.
 class CollegeConnect(models.Model):
+	profile = models.ForeignKey(to = Profile, on_delete="CASCADE")
 
-class Users(models.Model):
+class Profile(models.Model):
+	user = AutoOneToOneField(User, on_delete="CASCADE")
+	name = models.TextField()
 
 class Skill(models.Model):
 
